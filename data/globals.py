d5079ebf489c51d0378cbad71155a31e8d5f9c7e
Giuliano Albanese <albanesg@student.ethz.ch>
2021-12-09 21:50:02 +0000

    Implement before after image pair generation

---
+++ data/globals.py
@@ -0,0 +1,3 @@
+SAR_EXPORT_BANDS = ["VV_before", "VH_before", "VV_after", "VH_after"]
+MSO_EXPORT_BANDS = ["B4", "B3", "B2"]
+RESPONSES = ["deforestation_mask"]
---
+++ data/patch_sampler.py
@@ -0,0 +1,37 @@
+import ee
+import tensorflow as tf
+
+
+class PatchSampler:
+    def __init__(
+        self,
+        kernel_size: int,
+        scale: int,
+        num_samples_per_shard: int,
+        num_shards: int,
+    ):
+        lists = ee.List.repeat(ee.List.repeat(1, kernel_size), kernel_size)
+        self.kernel = ee.Kernel.fixed(kernel_size, kernel_size, lists)
+
+        self.scale = scale
+        self.num_shards = num_shards
+        self.num_samples_per_shard = num_samples_per_shard
+
+    def sample_image(self, image: ee.Image) -> ee.FeatureCollection:
+        array_image = image.float().neighborhoodToArray(self.kernel)
+        
+        # Export all the training data (in many pieces), with one task per image
+        samples = ee.FeatureCollection([])
+        for j in range(self.num_shards):
+            sample = array_image.sample(
+                region=None,
+                scale=self.scale,
+                numPixels=self.num_samples_per_shard,
+                seed=j,
+                tileScale=8,
+            )
+            samples = samples.merge(sample)
+
+        return samples
+
+        
